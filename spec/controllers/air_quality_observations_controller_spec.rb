05f98f5547ec756d0ac60d98cadd0ac32691bb7a
Brandon Mathis <bemathis@gmail.com>
2015-12-16 17:38:53 -0500

    Updates for linting

--- spec/controllers/air_quality_observations_controller_spec.rb
+++ spec/controllers/air_quality_observations_controller_spec.rb
@@ -8,14 +8,14 @@ describe Api::AirQualityObservationsController do
   end
 
   describe 'index' do
-    let!(:nearby_observations) {
+    let!(:nearby_observations) do
       create_list(:air_quality_observation, 5, reporting_area: "R'lyeh", lat: 40.5, lng: 47.5)
-    }
+    end
     let(:lat) { nearby_observations.first.lat + 1 }
     let(:lng) { nearby_observations.first.lng + 1 }
 
     it 'gets all data' do
-      get :index, { lat: lat, lng: lng }
+      get :index, lat: lat, lng: lng
       expect(
         json.map { |record| record['id'] }
       ).to eq nearby_observations.map(&:id)
@@ -31,8 +31,13 @@ describe Api::AirQualityObservationsController do
           lat: 40.5,
           lng: 47.5
         )
-        get :index, { lat: fresh_observations.first.lat, lng: fresh_observations.first.lng }
+        get :index, lat: fresh_observations.first.lat, lng: fresh_observations.first.lng
         expect(json.count).to eq 5
+        old_observations.each do |observation|
+          expect(
+            json.map { |record| record['id'] }
+          ).to_not include observation.id
+        end
       end
     end
   end
