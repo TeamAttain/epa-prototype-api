53b8af8e8b382fd4f7629f15e0d97c7b89af2b2c
Brandon Mathis <bemathis@gmail.com>
2015-12-24 16:19:17 -0500

    Gen bogus data test

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 describe Activity do
-  describe 'create' do
+  describe '.create' do
     subject { Activity.new(location: 'junk') }
 
     it 'validates that location is either inside our outside' do
       expect(subject).to be_invalid
       expect(subject.errors.keys).to include :location
+    end
+  end
+
+  describe '.gen_bogus_data!' do
+    before do
+      create_list(:air_quality_observation, 10, zip_code: AirnowApiAdapter.observed_zip_codes.sample)
+    end
+
+    it 'will generate bogus data for observed areas' do
+      expect { Activity.gen_bogus_data! }.to change { Activity.count }
     end
   end
 end
