02fb20071549d025f073e8379ad181a4094b52aa
Brandon Mathis <bemathis@gmail.com>
2015-12-24 15:47:56 -0500

    observations -> data in location query object

--- lib/location_proximity_query.rb
+++ lib/location_proximity_query.rb
@@ -1,8 +1,8 @@
 class LocationProximityQuery
-  attr_reader :observations, :zip_code, :lat, :lng
+  attr_reader :data, :zip_code, :lat, :lng
 
-  def initialize(observations, opts = {})
-    @observations = observations
+  def initialize(data, opts = {})
+    @data = data
     @lat = opts[:lat]
     @lng = opts[:lng]
     @zip_code = opts[:zip_code]
@@ -13,6 +13,6 @@ class LocationProximityQuery
   end
 
   def nearby
-    observations.within(100, origin: focal_point)
+    data.within(100, origin: focal_point)
   end
 end
